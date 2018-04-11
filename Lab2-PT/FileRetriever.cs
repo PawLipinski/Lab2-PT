fa43305f9517c84b2e1acf6ddb5c487533d06502
Paweł Lipiński <p.lipinski20@gmail.com>
2018-04-11 11:43:28 +0200

    Recursive load works

---
+++ Lab2-PT/FileRetriever.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace Lab2_PT
+{
+    class FileRetriever
+    {
+        private static List<MyTreeViewItem> items;
+
+        public static List<MyTreeViewItem> RetrieveFiles(string path)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            FileSystemInfo[] internals;
+
+            items = new List<MyTreeViewItem>();
+
+            //MyTreeViewItem analysedDirectory = new MyTreeViewItem();
+
+            try
+            {
+                if (di.Exists)
+                {
+                    internals = di.GetFileSystemInfos();
+
+                    foreach (var item in internals)
+                    {
+                        items.Add(RecursiveSubdirectoriesPrinter(item));
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No such directory");
+            }
+
+            return items;
+        }
+
+        private static MyTreeViewItem RecursiveSubdirectoriesPrinter(FileSystemInfo analysed, MyTreeViewItem parent = null)
+        {
+
+            if ((analysed.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                MyTreeViewItem toReturn = new MyTreeViewItem();
+                if (((DirectoryInfo)analysed).GetFileSystemInfos().Any())
+                {
+                    foreach (var item in ((DirectoryInfo)analysed).GetFileSystemInfos())
+                    {
+                        toReturn.Items.Add(RecursiveSubdirectoriesPrinter(item));
+                    }
+                }
+                toReturn.Header = analysed.Name;
+                toReturn.LinkPath = analysed.FullName;
+
+                return toReturn;
+            }
+
+            else
+            {
+                return new MyTreeViewItem() { Header = analysed.Name, LinkPath = analysed.FullName };
+            }
+        }
+    }
+}
--- Lab2-PT/MainWindow.xaml.cs
+++ Lab2-PT/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace Lab2_PT
 {
@@ -23,6 +25,30 @@ namespace Lab2_PT
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    var path = dialog.SelectedPath;
+                    filepathBox.Text = path;
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.treeView.Items.Clear();
+
+            List<MyTreeViewItem> results = FileRetriever.RetrieveFiles(filepathBox.Text);
+
+            foreach (var item in results)
+            {
+                this.treeView.Items.Add(item);
+            }
         }
     }
 }
